b579235056f981e994b41dce5ffcd843e79045e3
Akshay T <53436037+aksty@users.noreply.github.com>
2021-09-08 21:21:12 +0530

    add `useCreateIndex` and `useFindAndModify` to mongodb connection

--- models/connectDB.js
+++ models/connectDB.js
@@ -2,12 +2,21 @@ const mongoose = require("mongoose");
 const { configs } = require("../configs");
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(configs.MONGO_URI, {
-		useNewUrlParser: true,
-		//useCreateIndex: true,
-		//	useFindAndModify: false,
-		useUnifiedTopology: true,
-	});
+	let options;
+	if (configs.MONGO_URI === "mongodb://localhost:27017/") {
+		options = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		};
+	} else {
+		options = {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		};
+	}
+	const conn = await mongoose.connect(configs.MONGO_URI, options);
 
 	console.log(`MongoDB Connected`);
 };
