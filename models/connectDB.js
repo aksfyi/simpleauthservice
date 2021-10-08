68f9e948cc5e9c4a9b8f45201b3574a3ecc2ed7d
Akshay T <53436037+aksty@users.noreply.github.com>
2021-10-08 22:31:49 +0530

    fixed mongoose connection

--- models/connectDB.js
+++ models/connectDB.js
@@ -2,20 +2,11 @@ const mongoose = require("mongoose");
 const { configs } = require("../configs");
 
 const connectDB = async (fastify) => {
-	let options;
-	if (configs.MONGO_URI === "mongodb://localhost:27017/") {
-		options = {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		};
-	} else {
-		options = {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-			useUnifiedTopology: true,
-		};
-	}
+	let options = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+
 	const conn = await mongoose.connect(configs.MONGO_URI, options);
 
 	fastify.log.info("Connected to MongoDB");
