ff6134ad5cd8805b127a2dddd67cb477838d644b
Akshay T <53436037+aksty@users.noreply.github.com>
2022-07-10 10:49:18 +0530

    add sameSite "none" to refreshToken

--- models/refreshToken.js
+++ models/refreshToken.js
@@ -75,6 +75,7 @@ const getRefreshTokenOptns = () => {
 		httpOnly: true,
 		path: "/api/v1/auth/refresh",
 		signed: true,
+		sameSite: "none",
 	};
 	if (configs.ENVIRONMENT === keywords.PRODUCTION_ENV) {
 		options.secure = true;
